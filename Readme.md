75daaa1006a7618e090d40a113baac04b4ec12cd
surya3997 <surya3997@gmail.com>
2017-10-08 18:31:27 +0530

    update readme

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# Packet Looter  
+- Used to sniff packets from a network interface.  
+- **Requires sudo permission to capture in linux.**  
+- Displays submitted http form data.
--- packet_capture.py
+++ packet_capture.py
@@ -1,1 +1,10 @@
 import pyshark
+
+net_interface = 'wlan0'
+capture_time = 30
+
+capture = pyshark.LiveCapture(interface=net_interface)
+capture.sniff(timeout=capture_time)
+
+for i in range(len(capture)):
+    print(capture[i])
