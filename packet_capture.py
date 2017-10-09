4dc4a2a3cc923b62a5d4fb881f80f3540d6a945f
surya3997 <surya3997@gmail.com>
2017-10-09 18:29:40 +0530

    data encoded

--- packet_capture.py
+++ packet_capture.py
@@ -1,7 +1,8 @@
 import pyshark
+from base64 import b64encode as b64
 
 net_interface = 'wlan0'
-capture_time = 10
+capture_time = 20
 
 capture = pyshark.LiveCapture(interface=net_interface)
 capture.sniff(timeout=capture_time)
@@ -15,5 +16,6 @@ for i in range(len(capture)):
             print("Captured packet number : " + str(i + 1))
             print("Posted URL : ", packet.http.request_full_uri)
             print(packet["urlencoded-form"])
+            print("\n\nEncrypting the packet with base64 encryption: \n\n", b64(str(packet["urlencoded-form"]).encode()))
     except:
         pass
